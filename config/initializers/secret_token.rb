baff985958cb010105b0afb7e51a40fa3b4420bd
Patric Fornasier <patric.fornasier@gmail.com>
2010-10-20 22:51:25 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Splendid::Application.config.secret_token = '9023cbb9b99be1f64a6fe796dfbdd9ff1af8577fb144ee63c13ae1a955741a63f8aa38ba9c389c342eb2794ba2e61d2b42ea39ea8859d024b1ad3009e0ff853a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Splendid::Application.config.session_store :cookie_store, :key => '_splendid_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Splendid::Application.config.session_store :active_record_store
