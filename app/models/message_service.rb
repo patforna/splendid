613b5ee2d9b07432eb536c540b572144b98622da
Patric Fornasier <patric.fornasier@gmail.com>
2010-11-30 22:44:20 +0000

    implemented service test and wrote pending specs for xml builder and web client

--- app/models/message_service.rb
+++ app/models/message_service.rb
@@ -1,4 +1,11 @@
 class MessageService
+
+  def initialize
+    @builder = XmlBuilder.new
+    @webClient = WebClient.new
+  end
+
   def send(message)
+    @webClient.post @builder.from message
   end
 end
